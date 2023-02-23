8c5349db90a5875c93e7f0fc5d4e6212e4b2a34f
aprogrotess <1017208039@qq.com>
2023-02-23 09:05:26 +0800

    [common][cmake] 更新库文件和编译器的关系

--- ky_doc/online_book/.vuepress/route/sub/cmakeRoute.js
+++ ky_doc/online_book/.vuepress/route/sub/cmakeRoute.js
@@ -4,7 +4,8 @@ module.exports = [
             children: [
                 {title: 'CMAKE的简单使用', path: '/common/cmake/SimpleUse.md'},
                 {title: 'CMAKE组织子模块', path: '/common/cmake/SimpleLib.md'},
-                {title: 'CXX子模块间的调用', path: '/common/cmake/SimpleLibInteract.md'}
+                {title: 'CXX子模块间的调用', path: '/common/cmake/SimpleLibInteract.md'},
+                {title: '静态库动态库和编译器', path: '/common/cmake/LibSoCompile.md'}
             ]
         }
 ]
