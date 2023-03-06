cf8e247efc55d6de4f93865155cc94d13900e004
aprogrotess <1017208039@qq.com>
2023-03-06 23:06:46 +0800

    [develop][ai] 调整了ai的目录结构

--- ky_doc/online_book/.vuepress/route/sub/cmakeRoute.js
+++ ky_doc/online_book/.vuepress/route/sub/cmakeRoute.js
@@ -2,6 +2,7 @@ module.exports = [
         {
             title: '如何使用cmake',
             children: [
+                {title: 'CMAKE引入', path: '/common/cmake/'},
                 {title: 'CMAKE的简单使用', path: '/common/cmake/SimpleUse.md'},
                 {title: 'CMAKE组织子模块', path: '/common/cmake/SimpleLib.md'},
                 {title: 'CXX子模块间的调用', path: '/common/cmake/SimpleLibInteract.md'},
