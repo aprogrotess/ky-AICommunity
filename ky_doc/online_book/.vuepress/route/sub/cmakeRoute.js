85a662a0160773cd20728d8906703a1a89104e07
aprogrotess <1017208039@qq.com>
2023-03-06 22:28:29 +0800

    [common][cmake] route修正

--- ky_doc/online_book/.vuepress/route/sub/cmakeRoute.js
+++ ky_doc/online_book/.vuepress/route/sub/cmakeRoute.js
@@ -10,7 +10,8 @@ module.exports = [
                 {title: '发布CMAKE标准的静态库和动态库', path: '/common/cmake/SimpleCmakeInstall.md'},
                 {title: '链接常规的静态库和动态库', path: '/common/cmake/SimpleLink.md'},
                 {title: '发布CMAKE标准的静态库和动态库', path: '/common/cmake/SimpleCmakeLink.md'},
-                {title: '三方库的Compile', path: '/common/cmake/ThirdPartCompile.md'}
+                {title: '三方库的Compile', path: '/common/cmake/ThirdPartCompile.md'},
+                {title: '如何编写CMAKE的函数以及宏', path: '/common/cmake/FuncAndMarco.md'},
             ]
         }
 ]
